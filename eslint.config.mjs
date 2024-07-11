5e50fde53f8e52124a0b81c245a88ce6acd936fa
Rácz Zalán <zalan.racz.dev@gmail.com>
2024-07-12 00:48:31 +0200

    Adding ESLint configurations to make it compatible with the client

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,62 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict, {
-    files: ['**/*.ts'],
-    rules: {
-        '@typescript-eslint/no-unused-vars': 'warn',
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'enumMember',
+                    format: ['UPPER_CASE'],
+                },
+                {
+                    selector: 'typeLike',
+                    format: ['PascalCase'],
+                },
+                {
+                    selector: 'variableLike',
+                    format: ['camelCase'],
+                },
+                {
+                    selector: 'variable',
+                    modifiers: ['global', 'exported', 'const'],
+                    format: ['UPPER_CASE'],
+                },
+                {
+                    selector: 'parameter',
+                    modifiers: ['unused'],
+                    format: ['camelCase'],
+                    leadingUnderscore: 'allow',
+                },
+                {
+                    selector: 'property',
+                    format: ['camelCase'],
+                },
+                {
+                    selector: 'property',
+                    modifiers: ['readonly'],
+                    format: ['UPPER_CASE'],
+                },
+                {
+                    selector: 'method',
+                    format: ['camelCase'],
+                },
+            ],
+            '@typescript-eslint/semi': 'warn',
+            eqeqeq: ['error', 'smart'],
+            curly: 'warn',
+        },
     },
-});
+    {
+        ignores: ['node_modules', 'out', '**/*.js', '**/*.d.ts'],
+    }
+);
